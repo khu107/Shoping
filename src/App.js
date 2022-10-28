65e357df6992c143f2ead1d2324c4eceb98df9b3
metablog <khu10735@naver.com>
2022-10-28 23:45:24 +0900

    NavandCard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './mook/data';
+import Card from './Conponent/Card/Card';
 
 function App() {
+  const [shoese] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">MyShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+      <div className="container">
+        <div className="row">
+          {shoese.map((value, index) => {
+            return <Card shoese={shoese[index]} i={index + 1} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
+// function Card(props) {
+//   return (
+//     <div className="col-md-4">
+//       <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+//       <h4>{props.shoese[1].title}</h4>
+//       <p>{props.shoese[1].price}</p>
+//     </div>
+//   );
+// }
 
 export default App;
