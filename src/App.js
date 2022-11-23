8e955396755e8c5f34a1a2589916d495d4782bee
metablog <khu10735@naver.com>
2022-11-23 19:46:38 +0900

    qayta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import Cart from './Conponent/Cart/Cart';
+import data from './mook/data.js';
 
-function App() {
+function App(prop) {
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -14,35 +16,7 @@ function App() {
         </Container>
       </Navbar>
       <div className="main-bg"></div>
-
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <img
-              src="https://m.media-amazon.com/images/S/aplus-media/vc/b2d00dc2-c07c-4302-b827-c1e1c48f0f84._CR0,0,300,300_PT0_SX300__.jpg"
-              width="80%"
-            />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://beardoi.s3.ap-south-1.amazonaws.com/uploads/3382-2973-black-musk-eau-de-perfume-512x512.jpg"
-              width="80%"
-            />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://www.chanel.com/images//t_one/w_0.51,h_0.51,c_crop/q_auto:good,f_auto,fl_lossy,dpr_1.2/w_620/n-5-eau-de-parfum-spray-3-4fl-oz--packshot-default-125530-9518357119006.jpg"
-              width="80%"
-            />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-        </div>
-      </div>
+      <Cart />
     </div>
   );
 }
---
+++ src/Conponent/Cart/Cart.js
@@ -0,0 +1,13 @@
+export default function Cart() {
+  return (
+    <div className="container" style={{ border: '1px solid red' }}>
+      <div className="row">
+        <div className="col-md-4">
+          <img src="" width="80%" />
+          <h4>ree</h4>
+          <p>sds</p>
+        </div>
+      </div>
+    </div>
+  );
+}
